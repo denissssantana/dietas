a8ce36680183eea389ffb7741e560b1a65beb7e8
denissssantana <denissssantana@gmail.com>
2024-06-04 14:43:59 -0300

    feat - configuracao da responsividade tela home

---
+++ revel.js
@@ -0,0 +1,25 @@
+window.revelar = ScrollReveal({reset:true})
+
+// BANNER
+revelar.reveal('.efeito-banner',
+{
+    duration: 3000,
+    distance: '60px'
+})
+
+// TEXT
+revelar.reveal('.efeito-text',
+{
+    duration: 3000,
+    distance: '60px',
+    
+})
+
+// BTN
+revelar.reveal('.efeito-btn',
+{
+    duration: 3000,
+    distance: '60px', 
+    
+})
+
